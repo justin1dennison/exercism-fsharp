e2c9999c2a40a0b59850505423ae49eb109cd352
Justin Dennison <justin1dennison@gmail.com>
2020-06-02 12:57:57 -0400

    completed sum of multiples

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,7 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int = 
+    let ns = List.filter (fun n -> n <> 0) numbers;
+    [ for x in 1..(upperBound - 1) do for n in ns do if x % n = 0 then yield x ]
+    |> List.distinct
+    |> List.sum
