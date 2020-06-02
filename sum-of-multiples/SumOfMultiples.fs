7f3ad506302aae5ba5f88cb406973730e5f7436f
Justin Dennison <justin1dennison@gmail.com>
2020-06-02 13:10:01 -0400

    updated formatting on sum of multiples

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,7 +1,11 @@
-﻿module SumOfMultiples
+module SumOfMultiples
 
-let sum (numbers: int list) (upperBound: int): int = 
-    let ns = List.filter (fun n -> n <> 0) numbers;
-    [ for x in 1..(upperBound - 1) do for n in ns do if x % n = 0 then yield x ]
+
+
+let sum (numbers: int list) (upperBound: int): int =
+    let ns = List.filter (fun n -> n <> 0) numbers
+    [ for x in 1 .. (upperBound - 1) do
+        for n in ns do
+            if x % n = 0 then yield x ]
     |> List.distinct
     |> List.sum
