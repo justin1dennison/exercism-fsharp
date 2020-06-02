462737469aab952792af0b1e16b5b4ee602e9feb
Justin Dennison <justin1dennison@gmail.com>
2020-06-02 13:23:27 -0400

    updated two-fer

--- two-fer/TwoFer.fs
+++ two-fer/TwoFer.fs
@@ -1,6 +1,6 @@
-﻿module TwoFer
+module TwoFer
 
-let twoFer (input: string option): string = 
-    match input with
-    | Some(input) -> sprintf "One for %s, one for me." input
-    | None -> "One for you, one for me."
+let twoFer (input: string option): string =
+    input
+    |> Option.defaultValue "you"
+    |> sprintf "One for %s, one for me."
