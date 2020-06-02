d15a694ddcc2603843818ca5c8bdae0b4f74b2f0
Justin Dennison <justin1dennison@gmail.com>
2020-06-02 16:31:03 -0400

    refactor of pangram

--- pangram/Pangram.fs
+++ pangram/Pangram.fs
@@ -1,14 +1,20 @@
 module Pangram
+
 open System.Text.RegularExpressions
 
-let removeNumbers s = Regex.Replace(s, "[0-9]", "")
-let removePunctuations s = Regex.Replace(s, "[\"_\-.]", "")
-let removeSpaces s = Regex.Replace(s, " ", "")
-let clean = removeNumbers >> removeSpaces >> removePunctuations
+let space = " "
+let punctuations = "[\"_\-.]"
+let numbers = "[0-9]"
+let remove pattern s = Regex.Replace(s, pattern, "")
+
+let normalize =
+    remove numbers
+    >> remove punctuations
+    >> remove space
 
 let isPangram (input: string): bool =
     let letters =
         input.ToLower()
-        |> clean
+        |> normalize
         |> Set.ofSeq
     letters.Count = 26
