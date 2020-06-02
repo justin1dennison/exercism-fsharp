77d41f88fee07e66c5a58c88520d8f3b3dc6c9d0
Justin Dennison <justin1dennison@gmail.com>
2020-06-02 13:30:16 -0400

    completed difference of squares

---
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,12 @@
+module DifferenceOfSquares
+
+let square (number: int): int = number * number
+
+let squareOfSum (number: int): int =
+    [ 1 .. number ]
+    |> List.sum
+    |> square
+
+let sumOfSquares (number: int): int = [ 1 .. number ] |> List.sumBy square
+
+let differenceOfSquares (number: int): int = squareOfSum number - sumOfSquares number
