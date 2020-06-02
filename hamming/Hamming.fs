23eabbfe1049e04b69b91563b37cd2c810f298d2
Justin Dennison <justin1dennison@gmail.com>
2020-06-02 13:56:28 -0400

    completed hamming

---
+++ hamming/Hamming.fs
@@ -0,0 +1,11 @@
+module Hamming
+
+
+let distance (strand1: string) (strand2: string): int option =
+    if strand1.Length <> strand2.Length then
+        None
+    else
+        Seq.zip strand1 strand2
+        |> Seq.sumBy (fun (x, y) ->
+            if x = y then 0 else 1)
+        |> Some
