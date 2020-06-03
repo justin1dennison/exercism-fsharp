1bc4d44f958db82bd576351db1387e22be023731
Justin Dennison <justin1dennison@gmail.com>
2020-06-03 08:43:44 -0400

    updated reverse string

--- reverse-string/ReverseString.fs
+++ reverse-string/ReverseString.fs
@@ -1,6 +1,13 @@
 module ReverseString
 
-let reverse (input: string): string =
-    seq { input.Length - 1 .. -1 .. 0 }
-    |> Seq.map (fun x -> string (input.[x]))
-    |> Seq.fold (+) ""
+open System
+
+// This is a simpler solution than the one below. Something to remember.
+let reverse (input: string): string = input |> Seq.rev |> String.Concat
+
+
+//let reverse (input: string): string =
+//    seq { input.Length - 1 .. -1 .. 0 }
+//    |> Seq.map (fun x -> string (input.[x]))
+//    |> Seq.fold (+) ""
+
