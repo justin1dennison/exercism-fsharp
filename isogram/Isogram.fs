73053058d8f155f22cd6968d26f965bb1cb0e6e0
Justin Dennison <justin1dennison@gmail.com>
2020-06-03 15:15:47 -0400

    simplified the isogram soultion using System.Char.IsLetter

--- isogram/Isogram.fs
+++ isogram/Isogram.fs
@@ -1,11 +1,5 @@
 module Isogram
 
 let isIsogram (str: string): bool =
-    let charSeq =
-        str.ToLower()
-        |> Seq.filter (fun x -> x <> '-')
-        |> Seq.filter (fun x -> x <> ' ')
-
-    let charSet = charSeq |> Set.ofSeq
-
-    Set.count charSet = Seq.length charSeq
+    let ns = str.ToLower() |> String.filter System.Char.IsLetter
+    (set ns |> Set.count) = String.length ns
