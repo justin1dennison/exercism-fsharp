34ea52b42bd362be90259c2ee92ef19b8485ec72
Justin Dennison <justin1dennison@gmail.com>
2020-06-04 19:02:22 -0400

    changed to use the Map type

--- rna-transcription/RnaTranscription.fs
+++ rna-transcription/RnaTranscription.fs
@@ -1,15 +1,11 @@
 module RnaTranscription
 
-let transcribe (codon: char ): char =
-    match codon with
-    | 'a'
-    | 'A' -> 'U'
-    | 'c'
-    | 'C' -> 'G'
-    | 't'
-    | 'T' -> 'A'
-    | 'g'
-    | 'G' -> 'C'
-    | _ -> failwith "Invalid codon"
+let transcriber =
+    Map
+        [ 'A', 'U'
+          'C', 'G'
+          'T', 'A'
+          'G', 'C' ]
 
+let transcribe (codon: char): char = transcriber.[codon]
 let toRna (dna: string): string = dna |> String.map transcribe
