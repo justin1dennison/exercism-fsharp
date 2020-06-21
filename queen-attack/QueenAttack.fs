520df804df4f6e022a295868fd3d6cd51ca5f9ea
Justin Dennison <justin1dennison@gmail.com>
2020-06-21 12:27:10 -0400

    refactor queen attack to remove inappropriate match statement

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -12,9 +12,7 @@ let create (position: int * int): bool =
 let canAttack (queen1: int * int) (queen2: int * int): bool =
     let (x1, y1) = queen1
     let (x2, y2) = queen2
-
-    let inPosition =
-        match ((y2 - y1), (x2 - x1)) with
-        | (x, y) when x = 0 || y = 0 || x / y = -1 || x / y = 1 -> true
-        | _ -> false
+    let dx = (x2 - x1)
+    let dy = (y2 - y1)
+    let inPosition = dx = 0 || dy = 0 || dx / dy = -1 || dx / dy = 1
     inPosition && create queen1 && create queen2
