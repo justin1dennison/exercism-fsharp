afcac9a40f9d70ea9d260b6cc55e1b9671bb815d
Justin Dennison <justin1dennison@gmail.com>
2020-07-01 13:37:30 -0400

    refactor based on mentor for QueenAttack

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -2,17 +2,20 @@ module QueenAttack
 
 let boardBounds = (0, 7)
 
-let (<=>) x (min, max): bool = x <= max && x >= min
-
+let between x (min, max): bool = x <= max && x >= min
 
 let create (position: int * int): bool =
-    let (x, y) = position
-    x <=> boardBounds && y <=> boardBounds
+    let x, y = position
+    between x boardBounds && between y boardBounds
 
 let canAttack (queen1: int * int) (queen2: int * int): bool =
-    let (x1, y1) = queen1
-    let (x2, y2) = queen2
-    let dx = (x2 - x1)
-    let dy = (y2 - y1)
-    let inPosition = dx = 0 || dy = 0 || dx / dy = -1 || dx / dy = 1
+    let x1, y1 = queen1
+    let x2, y2 = queen2
+    let dx = x2 - x1
+    let dy = y2 - y1
+    let onDiagonalLine = dy <> 0 && (dx / dy = -1 || dx / dy = 1)
+    let onVerticalLine = dx = 0
+    let onHorizontalLine = dy = 0
+    let inPosition = onVerticalLine || onHorizontalLine || onDiagonalLine
+
     inPosition && create queen1 && create queen2
